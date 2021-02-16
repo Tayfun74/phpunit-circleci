7e308e92b9f2e61884dfd27a45dc40b6d194a3fb
Tayfun Aydin <tayfunaydin1996@gmail.com>
2021-02-16 18:57:14 +0100

    a

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Tests\Controller;
+
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testEnclosuresAreShownOnHomepage()
+    {
+        $client = $this->makeClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertStatusCode(200, $client);
+    }
+}
